09446889f21e5230cfd1779eee11022593c0ec7d
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-04-12 10:28:07 +0700

    feat: about: add link to github

--- composeApp/src/wasmJsMain/kotlin/com/singularityuniverse/webpage/application/About.kt
+++ composeApp/src/wasmJsMain/kotlin/com/singularityuniverse/webpage/application/About.kt
@@ -7,9 +7,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.onClick
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -17,6 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,6 +37,7 @@ class About : Application() {
     override val title: String = "About"
     override val defaultMinSize: DpSize = DpSize(300.dp, 400.dp)
 
+    @OptIn(ExperimentalFoundationApi::class)
     @Composable
     override fun Draw(modifier: Modifier) {
         Column(
@@ -53,6 +61,21 @@ class About : Application() {
                 text = "v0.0.0-proto",
                 style = MaterialTheme.typography.overline
             )
+            Spacer(Modifier.height(8.dp))
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.secondary,
+                    contentColor = MaterialTheme.colors.onSecondary
+                ),
+                onClick = {
+                    kotlinx.browser.window.open("https://github.com/SingularityIndonesia/SingularityWebAppSrc")
+                }
+            ) {
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = "Source Code",
+                )
+            }
         }
     }
 
