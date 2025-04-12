c8e72371b82e7224b71c5d2dde457ce2a95f4ec8
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-04-12 19:06:05 +0700

    feat: magician: update

--- composeApp/src/commonMain/kotlin/com.singularityuniverse.webpage/application/Magician.kt
+++ composeApp/src/commonMain/kotlin/com.singularityuniverse.webpage/application/Magician.kt
@@ -25,90 +25,95 @@ class Magician : Application() {
 
     @Composable
     override fun Draw(modifier: Modifier) {
-        Column(
+        Box(
             modifier = modifier,
-            horizontalAlignment = Alignment.CenterHorizontally,
+            contentAlignment = Alignment.Center
         ) {
-            Spacer(modifier = Modifier.height(24.dp))
-            Spacer(modifier = Modifier.height(24.dp))
-            Image(
-                modifier = Modifier
-                    .size(120.dp)
-                    .clip(RoundedCornerShape(120.dp)),
-                alignment = Alignment.Center,
-                contentScale = ContentScale.Crop,
-                painter = painterResource(Res.drawable.magician_profile_pict),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-            Text(
-                text = "Stefanus Ayudha",
-                style = MaterialTheme.typography.h5
-            )
-            Text(
-                text = "Kotlin Developer Expert",
-                style = MaterialTheme.typography.body2
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-            TextButton(
-                onClick = {
-                    window.open("https://wa.me/+62895340952006")
-                }
+            Column(
+                modifier = Modifier.wrapContentSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text("Contact the Magician")
-            }
-            Row (
-                verticalAlignment = Alignment.CenterVertically
-            ){
-                IconButton(
-                    onClick = {
-                        window.open("mailto:stefanus.ayudha@gmail.com")
-                    }
-                ) {
-                    Image(
-                        modifier = Modifier.requiredHeight(20.dp),
-                        contentScale = ContentScale.FillHeight,
-                        painter = painterResource(Res.drawable.ic_gmail),
-                        contentDescription = null
-                    )
-                }
-                IconButton(
-                    onClick = {
-                        window.open("https://github.com/stefanusayudha")
-                    }
-                ) {
-                    Image(
-                        modifier = Modifier.requiredHeight(24.dp),
-                        contentScale = ContentScale.FillHeight,
-                        painter = painterResource(Res.drawable.ic_github),
-                        contentDescription = null
-                    )
-                }
-                IconButton(
-                    onClick = {
-                        window.open("https://www.linkedin.com/in/stefanus-ayudha-447a98b5/")
-                    }
-                ) {
-                    Image(
-                        modifier = Modifier.requiredHeight(24.dp),
-                        contentScale = ContentScale.FillHeight,
-                        painter = painterResource(Res.drawable.ic_linkedin),
-                        contentDescription = null
-                    )
-                }
-                IconButton(
+                Spacer(modifier = Modifier.height(24.dp))
+                Image(
+                    modifier = Modifier
+                        .size(120.dp)
+                        .clip(RoundedCornerShape(120.dp)),
+                    alignment = Alignment.Center,
+                    contentScale = ContentScale.Crop,
+                    painter = painterResource(Res.drawable.magician_profile_pict),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Text(
+                    text = "Stefanus Ayudha",
+                    style = MaterialTheme.typography.h5
+                )
+                Text(
+                    text = "Kotlin Developer Expert",
+                    style = MaterialTheme.typography.body2
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+                TextButton(
                     onClick = {
                         window.open("https://wa.me/+62895340952006")
                     }
                 ) {
-                    Image(
-                        modifier = Modifier.requiredHeight(28.dp),
-                        contentScale = ContentScale.FillHeight,
-                        painter = painterResource(Res.drawable.ic_whatsapp),
-                        contentDescription = null
-                    )
+                    Text("Contact the Magician")
                 }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(
+                        onClick = {
+                            window.open("mailto:stefanus.ayudha@gmail.com")
+                        }
+                    ) {
+                        Image(
+                            modifier = Modifier.requiredHeight(20.dp),
+                            contentScale = ContentScale.FillHeight,
+                            painter = painterResource(Res.drawable.ic_gmail),
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            window.open("https://github.com/stefanusayudha")
+                        }
+                    ) {
+                        Image(
+                            modifier = Modifier.requiredHeight(24.dp),
+                            contentScale = ContentScale.FillHeight,
+                            painter = painterResource(Res.drawable.ic_github),
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            window.open("https://www.linkedin.com/in/stefanus-ayudha-447a98b5/")
+                        }
+                    ) {
+                        Image(
+                            modifier = Modifier.requiredHeight(24.dp),
+                            contentScale = ContentScale.FillHeight,
+                            painter = painterResource(Res.drawable.ic_linkedin),
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            window.open("https://wa.me/+62895340952006")
+                        }
+                    ) {
+                        Image(
+                            modifier = Modifier.requiredHeight(28.dp),
+                            contentScale = ContentScale.FillHeight,
+                            painter = painterResource(Res.drawable.ic_whatsapp),
+                            contentDescription = null
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(24.dp))
             }
         }
     }
